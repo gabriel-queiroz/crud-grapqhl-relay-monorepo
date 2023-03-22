e80956af56b10f7ad85b05d21f1abe85868a9ff4
gabriel-queiroz <gabrielqueirozzn@gmail.com>
2023-03-22 16:42:01 -0300

    feat(front): Create React App

---
+++ apps/front/src/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+export const App = () => (
+  <div>
+    <GlobalStyle />
+    <h1>monorepo crud</h1>
+  </div>
+);
---
+++ apps/front/webpack.config.js
@@ -0,0 +1,37 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./public"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack Boilerplate",
+      template: path.resolve(__dirname, "./public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+};
