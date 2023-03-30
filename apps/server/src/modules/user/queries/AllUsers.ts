6b17b1d97c04bcbf0ad277814d11203ddccd2498
gabriel-queiroz <gabrielqueirozzn@gmail.com>
2023-03-30 11:24:19 -0300

    feat: Add pagination of users

--- apps/server/src/modules/user/queries/AllUsers.ts
+++ apps/server/src/modules/user/queries/AllUsers.ts
@@ -1,6 +1,14 @@
 import { UserModel } from '../UserModel'
 import * as graphql from 'graphql'
 
+const DEFAULT_PAGE = 1
+const DEFAULT_PAGE_SIZE = 10
+
+const DEFAULT_PAGINATION = {
+  page: DEFAULT_PAGE,
+  pageSize: DEFAULT_PAGE_SIZE
+}
+
 const userType = new graphql.GraphQLObjectType({
   name: 'User',
   fields: {
@@ -10,9 +18,49 @@ const userType = new graphql.GraphQLObjectType({
     city: { type: new graphql.GraphQLNonNull(graphql.GraphQLString) }
   }
 })
+
+const usersType = new graphql.GraphQLObjectType({
+  name: 'Users',
+  fields: {
+    totalPages: {
+      type: graphql.GraphQLInt
+    },
+    currentPage: {
+      type: graphql.GraphQLInt
+    },
+    totalItems: {
+      type: graphql.GraphQLInt
+    },
+    users: {
+      type: new graphql.GraphQLList(userType)
+    }
+  }
+})
+
 export const allUsers = {
   users: {
-    type: new graphql.GraphQLList(userType),
-    resolve: async () => await UserModel.find({}).lean()
+    args: {
+      page: {
+        type: graphql.GraphQLInt
+      },
+      pageSize: {
+        type: graphql.GraphQLInt
+      }
+    },
+    type: usersType,
+    resolve: async (data = DEFAULT_PAGINATION) => {
+      const skipAmount = (data.page - 1) * data.pageSize
+      const count = await UserModel.count()
+      const users = UserModel.find({})
+        .skip(skipAmount)
+        .limit(data.pageSize * 1)
+        .lean()
+      return {
+        data: users,
+        totalPages: Math.ceil(count / data.pageSize),
+        currentPage: data.page,
+        totalItems: count
+      }
+    }
   }
 }
